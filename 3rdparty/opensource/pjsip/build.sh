331cfe32d0b29e8586ef7bbb2d456620d9a59880
txgcwm <txgcwm@163.com>
2017-08-20 03:32:37 +0800

    update

--- 3rdparty/opensource/pjsip/build.sh
+++ 3rdparty/opensource/pjsip/build.sh
@@ -23,11 +23,11 @@ tar -jxvf ${FILENAME}
 
 # build lib
 cd ${DIRNAME}
-./configure --prefix=${CURDIR}/${INSTALLDIR} --disable-sound --disable-oss --disable-video \
+./configure --prefix=${CURDIR}/${INSTALLDIR} --build=i686-linux --disable-sound --disable-oss --disable-video \
 --disable-small-filter --disable-large-filter --disable-speex-aec --disable-g711-codec \
 --disable-l16-codec --disable-gsm-codec --disable-g722-codec --disable-g7221-codec \
 --disable-speex-codec --disable-ilbc-codec --disable-sdl --disable-ffmpeg --disable-v4l2 \
---disable-openh264 --disable-webrtc --disable-libyuv --disable-shared
+--disable-openh264 --disable-libwebrtc --disable-libyuv --disable-shared
 make
 make install
 cd ..
